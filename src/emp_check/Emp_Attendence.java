3f6e87d9ec7ac159ab4b4adffcca22b229e76044
Your Name <you@example.com>
2022-06-20 23:04:38 +0530

    added welcome message

--- src/emp_check/Emp_Attendence.java
+++ src/emp_check/Emp_Attendence.java
@@ -3,7 +3,7 @@ package emp_check;
 public class Emp_Attendence {
 
 	public static void main(String[] args) {
-		System.out.println("welocime to employee wage computation");
+		System.out.println("welcome to employee wage computation");
 	}
 
 }
