d50d0454dc269e738f4c6d2175672f21939ed449
Your Name <you@example.com>
2022-06-20 23:55:10 +0530

    employee monthly calculation using switch case is added

--- src/emp_check/Emp_Attendence.java
+++ src/emp_check/Emp_Attendence.java
@@ -7,17 +7,30 @@ public class Emp_Attendence {
 		int wage_per_hour = 20;
 		int full_day_hour = 8;
 		int part_time_hour = 4;
+		int month = 20;
 		System.out.println("welcome to employee wage computation");
-		double emp_check = Math.floor(Math.random() * 10) % 3;
+		int emp_check = (int) (Math.floor(Math.random() * 10) % 3);
 		if (emp_check == emp_present) {
 			System.out.println("The employee is present");
 			System.out.println("The wage is : " + wage_per_hour * full_day_hour);
 		} else if (emp_check == 2) {
-			System.out.println("the employee is absent");
-		}
-		else {
 			System.out.println("The employee is part time");
 			System.out.println("the employee wage is : " +wage_per_hour*part_time_hour);
+		}
+		else {
+			System.out.println("the employee is absent");
+		}
+		switch (emp_check) {
+		case 1:
+			System.out.println("The employee is present");
+			System.out.println("The wage for the month is : " + wage_per_hour * full_day_hour * month);
+			break;
+		case 2:
+			System.out.println("The employee is present part time");
+			System.out.println("The wage for the month is : " + wage_per_hour * part_time_hour * month);
+			break;
+		default:
+			System.out.println("the employee is absent");
 		}
 	}
 
