1bd25bca3a83328cb6567ff144d928fcd2baf025
Your Name <you@example.com>
2022-06-20 23:12:28 +0530

    employee absent or present is included

--- src/emp_check/Emp_Attendence.java
+++ src/emp_check/Emp_Attendence.java
@@ -3,7 +3,15 @@ package emp_check;
 public class Emp_Attendence {
 
 	public static void main(String[] args) {
+		int emp_present = 1;
 		System.out.println("welcome to employee wage computation");
+		double emp_check = Math.floor(Math.random() * 10) % 2;
+		if (emp_check == emp_present) {
+			System.out.println("The employee is present");
+
+		} else {
+			System.out.println("the employee is absent");
+		}
 	}
 
 }
