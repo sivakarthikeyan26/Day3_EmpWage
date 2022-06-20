16170340e9cbab11e76c5027ed059579f061816f
Your Name <you@example.com>
2022-06-20 23:34:16 +0530

    part time calculation added

--- src/emp_check/Emp_Attendence.java
+++ src/emp_check/Emp_Attendence.java
@@ -6,13 +6,18 @@ public class Emp_Attendence {
 		int emp_present = 1;
 		int wage_per_hour = 20;
 		int full_day_hour = 8;
+		int part_time_hour = 4;
 		System.out.println("welcome to employee wage computation");
-		double emp_check = Math.floor(Math.random() * 10) % 2;
+		double emp_check = Math.floor(Math.random() * 10) % 3;
 		if (emp_check == emp_present) {
 			System.out.println("The employee is present");
 			System.out.println("The wage is : " + wage_per_hour * full_day_hour);
-		} else {
+		} else if (emp_check == 2) {
 			System.out.println("the employee is absent");
+		}
+		else {
+			System.out.println("The employee is part time");
+			System.out.println("the employee wage is : " +wage_per_hour*part_time_hour);
 		}
 	}
 
