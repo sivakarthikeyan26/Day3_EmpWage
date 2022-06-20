e8e4f0e95267d01c16ac414994c647e829728cd4
Your Name <you@example.com>
2022-06-20 23:03:28 +0530

    added welcome message

--- src/emp_check/Emp_Attendence.java
+++ src/emp_check/Emp_Attendence.java
@@ -3,8 +3,7 @@ package emp_check;
 public class Emp_Attendence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("welocime to employee wage computation");
 	}
 
 }
